842e4b501969e429971ff9d31f2c6d676de50e42
009-KumarJi <krishnakumar091@yahoo.com>
2024-09-04 02:45:03 +0530

    @PostConstruct

--- src/main/java/com/krishna/vyzionix/impl/VideoServiceImpl.java
+++ src/main/java/com/krishna/vyzionix/impl/VideoServiceImpl.java
@@ -2,11 +2,13 @@ package com.krishna.vyzionix.impl;
 
 import com.krishna.vyzionix.entities.Video;
 import com.krishna.vyzionix.services.VideoService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
@@ -18,6 +20,17 @@ public class VideoServiceImpl implements VideoService {
 
     @Value("${video.upload.dir}")
     String DIR;
+
+    @PostConstruct
+    public void init() {
+        File dir = new File(DIR);
+        if (!dir.exists()) {
+            dir.mkdir();
+            System.out.println("Directory created: " + DIR);
+        } else {
+            System.out.println("Directory already exists: " + DIR);
+        }
+    }
 
     @Override
     public Video saveVideo(Video video, MultipartFile file) {
@@ -33,11 +46,8 @@ public class VideoServiceImpl implements VideoService {
 
             Path path = Paths.get(cleanDirectory, cleanFileName); // file_path
 
-
-
-
-
-
+            // Save the file
+            System.out.println("File Path: " + path);
 
         } catch (IOException e) {
             e.printStackTrace();
